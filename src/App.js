bf90f7e031e47cc2707b486dd1e23b1599a78129
Arihant Jain <arihantjaingit02@gmail.com>
2023-04-15 19:45:16 +0530

    file paths corrected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import {Routes,Route} from "react-router-dom"
-import Signup from "../erp-ui/src/routes/Signup"
-import Alumni from "../erp-ui/src/routes/Alumni"
-import Fees from "../erp-ui/src/routes/Fees"
-import Placements from "../erp-ui/src/routes/Placement"
-import Profile from "../erp-ui/src/routes/Profile"
-import Navbar from '../erp-ui/src/components/navbar';
+import Signup from "./routes/Signup"
+import Alumni from "./routes/Alumni"
+import Fees from "./routes/Fees"
+import Placements from "./routes/Placement"
+import Profile from "./routes/Profile"
+import Navbar from './components/navbar';
 import React from 'react';
 import './App.css';
 function App() {
