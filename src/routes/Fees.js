e6f2a09b67121590ec7b7c47b46456e367babc5a
Arsh <singhalarsh2001@gmail.com>
2023-04-19 22:14:35 +0530

    erp-ui

--- src/routes/Fees.js
+++ src/routes/Fees.js
@@ -9,7 +9,20 @@ function Fees() {
     <Header/>
     <Todaycircular/>
     <Navbar/>
-      
+    <div className = "fees-main">
+      <div className = "fees-text">
+        <h3>DETAILS:</h3>
+        <p className='nme'>Name:</p>
+        <p className='nme'>Course:</p>
+        <p className='nme'>Specialization:</p>
+        <p className='nme'> Balance Fee Amount:</p>
+      </div>
+        <div className ="fee-payment">
+            <form id='fee_payment' method="post">
+            <button id="login_btn1" type="submit">PAY</button>
+            </form>
+        </div>
+      </div>
     </div>
   )
 }
