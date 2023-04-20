362ba626d6afe42c2c56ff5bf890415db22919f7
Arsh <singhalarsh2001@gmail.com>
2023-04-20 15:51:23 +0530

    profile section added

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,58 @@
 import React from 'react'
 import Header from "../components/Header"
 import Navbar from "../components/navbar"
-import Todaycircular from "../components/Today_circular.js"
+import "../components/styles/Profile.css"
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
 function Profile() {
   return (
     <div>
     <Header/>
-    <Todaycircular/>
     <Navbar/>
-      <p> hello</p>
-    </div>
-  )
-}
+    
+      <MDBContainer>
+        <MDBRow className="justify-content-center">
+          <MDBCol md="9" lg="7" xl="5" className="mt-5">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="p-4">
+                <div className="d-flex text-black">
+                  <div className="flex-shrink-0">
+                    <MDBCardImage
+                      style={{ width: '180px', borderRadius: '10px' }}
+                      src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp'
+                      alt='Generic placeholder image'
+                      fluid />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                    <MDBCardTitle>Danny McLoan</MDBCardTitle>
+                    <MDBCardText>Senior Journalist</MDBCardText>
 
-export default Profile
+                    <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
+                      style={{ backgroundColor: '#efefef' }}>
+                      <div>
+                        <p className="small text-muted mb-1">Articles</p>
+                        <p className="mb-0">41</p>
+                      </div>
+                      <div className="px-3">
+                        <p className="small text-muted mb-1">Followers</p>
+                        <p className="mb-0">976</p>
+                      </div>
+                      <div>
+                        <p className="small text-muted mb-1">Rating</p>
+                        <p className="mb-0">8.5</p>
+                      </div>
+                    </div>
+                    <div className="d-flex pt-1">
+                      <MDBBtn outline className="me-1 flex-grow-1">Chat</MDBBtn>
+                      <MDBBtn className="flex-grow-1">Follow</MDBBtn>
+                    </div>
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    <div className="vh-100" style={{ backgroundColor: '#9de2ff' }}>
+    </div>
+     </div> 
+)}export default Profile
